2e3a6d5372ed017cf6a95bef183eb787e94ec818
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-09-21 14:59:08 -0700

    Added strings that contains various options on a module scale.

--- Notes/RFER.md
+++ Notes/RFER.md
@@ -4,3 +4,7 @@
 - Capturing printed strings
 >> https://stackoverflow.com/a/20507769
 >> https://docs.pytest.org/en/7.1.x/how-to/capture-stdout-stderr.html
+
+# RFER 02
+- Example of calling json from github
+>> https://gist.github.com/sirleech/2660189
--- cli_offline/tests/test_functional.py
+++ cli_offline/tests/test_functional.py
@@ -4,6 +4,13 @@ from pytest import CaptureFixture
 from src.ui import user_interface
 
 messenge_purposely_failed:str = 'failed on purpose'
+welcome_message:str = "Welcome to Project miru 『見る』! Type in one of the options to continue :3"
+option_all_random_anime:str = "Give me any anime that exists!"
+option_select_from_user_list:str = "Give me a random anime of my list!"
+option_list_all_my_lists:str = "List all of my anime!"
+option_download_database:str = "Download the database!"
+
+
 class Tests_user_cli:
     
 
@@ -14,9 +21,9 @@ class Tests_user_cli:
         # Mark opens the application for the first time, and is greeted with the options they can interact with. 
         ui.start()
         captured: CaptureFixture[str] = capsys.readouterr()
-        assert "Welcome to Project miru 『見る』! Type in one of the options to continue :3" in captured._captured_out # TODO - Check if this fixture works as like "captured.out"
+        assert welcome_message in captured._captured_out # TODO - Check if this fixture works as like "captured.out"
 
-        # Mark decides that he wants to obtain a random anime to watch. However, the prompt asks whether or not 
+        # Mark decides that he wants to obtain a random anime to watch. However, the application (app) detects that this is Mark's first time launching the program. The prompt asks whether or not 
 
         
         
