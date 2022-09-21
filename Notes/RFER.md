0196c5c811750f7d012ee29d8e1fb82809144eb8
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-09-21 14:52:16 -0700

    Wrote TDD for launch and added init files for pytest

---
+++ Notes/RFER.md
@@ -0,0 +1,6 @@
+
+
+# RFER 01
+- Capturing printed strings
+>> https://stackoverflow.com/a/20507769
+>> https://docs.pytest.org/en/7.1.x/how-to/capture-stdout-stderr.html
--- cli_offline/tests/test_functional.py
+++ cli_offline/tests/test_functional.py
@@ -1,14 +1,20 @@
+from email.generator import Generator
 import pytest
-
+from pytest import CaptureFixture
 from src.ui import user_interface
 
 messenge_purposely_failed:str = 'failed on purpose'
 class Tests_user_cli:
+    
 
-    def test_executing_rolling_random_title_from_overall_anime_database(self):
-        
+    def test_executing_rolling_any_random_title_from_overall_anime_database(self, capsys:CaptureFixture):
+        ui: user_interface = user_interface()
+
+
         # Mark opens the application for the first time, and is greeted with the options they can interact with. 
-        assert "Welcome! Type in one of the options!" in 
+        ui.start()
+        captured: CaptureFixture[str] = capsys.readouterr()
+        assert "Welcome to Project miru 『見る』! Type in one of the options to continue :3" in captured._captured_out # TODO - Check if this fixture works as like "captured.out"
 
         # Mark decides that he wants to obtain a random anime to watch. However, the prompt asks whether or not 
 
@@ -18,4 +24,8 @@ class Tests_user_cli:
         
         
         # Purposely fail
+        assert None == messenge_purposely_failed
+    
+    def test_executing_grabbing_random_anime_from_user_list(self, capsys):
+
         assert None == messenge_purposely_failed
