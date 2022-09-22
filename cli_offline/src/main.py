82c77a6a322a6014f528c72a80ded3f303183080
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-09-21 17:24:03 -0700

    Finished first section of user story; wrote template of main()

---
+++ cli_offline/src/main.py
@@ -0,0 +1,7 @@
+
+
+def main():
+    print("nothing here sadge :'[")
+
+if __name__ == "__main__":
+    main()
--- cli_offline/tests/test_functional.py
+++ cli_offline/tests/test_functional.py
@@ -3,12 +3,14 @@ import pytest
 from pytest import CaptureFixture
 from src.ui import user_interface
 
-messenge_purposely_failed:str = 'failed on purpose'
-welcome_message:str = "Welcome to Project miru 『見る』! Type in one of the options to continue :3"
-option_all_random_anime:str = "Give me any anime that exists!"
-option_select_from_user_list:str = "Give me a random anime of my list!"
-option_list_all_my_lists:str = "List all of my anime!"
-option_download_database:str = "Download the database!"
+message_purposely_failed:str = 'failed on purpose'
+
+welcome_message:str = "Welcome to Project miru 『見る』! Type in one of the options to continue :3\n"
+
+option_all_random_anime:str = "Give me any anime that exists!\n"
+option_select_from_user_list:str = "Give me a random anime of my list!\n"
+option_list_all_my_lists:str = "List all of my anime!\n"
+option_download_database:str = "Download the database!\n"
 
 
 class Tests_user_cli:
@@ -23,16 +25,27 @@ class Tests_user_cli:
         captured: CaptureFixture[str] = capsys.readouterr()
         assert welcome_message in captured._captured_out # TODO - Check if this fixture works as like "captured.out"
 
-        # Mark decides that he wants to obtain a random anime to watch. However, the application (app) detects that this is Mark's first time launching the program. The prompt asks whether or not 
+        # Mark decides that he wants to obtain a random anime to watch. However, the app (application) detects that this is Mark's first time launching the program. The program asks if they want to download the json of the anime database.
 
-        
-        
+
+        # Mark accidentally said no. The application displayed a message saying that to either manually download the anime database (with link to GitHub) or let the program download the Json. Then, the application returns back to the menu.
+
+            
+        # Mark decided to say yes to download the file. The program will download the json if Mark types yes OR Mark timed out after 5 seconds.
+            ## The program will download the json if Mark types yes OR Mark timed out after 5 seconds.
+            ## If download failed, pop a 5 second failed-message and only return back to the options list.
         
         
         
         # Purposely fail
-        assert None == messenge_purposely_failed
+        assert None == message_purposely_failed
     
     def test_executing_grabbing_random_anime_from_user_list(self, capsys):
 
-        assert None == messenge_purposely_failed
+        # Mark decides that he wants to obtain a random anime to watch. However, the application (app) detects that this is Mark's first time launching the program. The program asks if they want to download the json of the anime database. The program will download the json if Mark types yes OR Mark timed out after 5 seconds.
+            
+            ## If Mark said no, the application will default to linking the anime's raw ID and MAL (MyAnimeList) link from Mark's list.
+
+            ## If download failed, pop a 5 second message and only return the raw ID and MAL link.
+
+        assert None == message_purposely_failed
