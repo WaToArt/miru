c8c889ead4a0b7a7269fcae74c2dd18a893d044e
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-11-01 03:40:40 -0700

    tested with grabbing first anime entry

--- cli_offline/src/main.py
+++ cli_offline/src/main.py
@@ -4,8 +4,9 @@ def main():
     download_db:download_anime_database_json = download_anime_database_json()
     anime_json:dict = download_db.download_json()
     repo = anime_json["repository"]
-    
-    print()
+    first_anime_entry = anime_json["data"][0]
+
+    print(first_anime_entry)
 
 if __name__ == "__main__":
     main()
