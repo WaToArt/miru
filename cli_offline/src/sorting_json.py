3369cf6c601e2f32b3b6239441d4607a501cfcfe
WaToArt <93100367+WaToArt@users.noreply.github.com>
2022-11-13 17:09:09 -0800

    sorting works, but it's not sorting by MAL

--- cli_offline/src/sorting_json.py
+++ cli_offline/src/sorting_json.py
@@ -6,14 +6,28 @@ class sorting_json:
         pass
 
     def sort_by_MAL_url(self, response_json):
-        output_json:dict = response_json
 
-        ### Finish writing code #TODO
+        """### Finish writing code #TODO
 
-        output_json = sorted(output_json.items(), key=lambda item: item['data']['sources'])
+        - obtain data from json and assign to variable 'dict_json_data'
+            - since json is a list, sort by MAL url
+        - replace 'response_json''s 'data' with sorted 'dict_json_data' 
+
+        """
+        print("\n\n ====== assigning variable for json's data ======\n")
+        dict_json_data:list = (response_json['data'])
+        print(f"assigned content of 'data' to variable 'dict_json_data'")
+        
+        print("\n\n=== sorted data === \n")
+
+        dict_json_data.sort(key=lambda item: item['sources'])
+        print(json.dumps(dict_json_data, indent=2))
+        
+        print("\n\n ====== output_json ====== \n")
+        output_json = dict_json_data
         print(output_json)
 
         print("\n=======new line========\n")
 
-        print(json.dumps(output_json, indent=4))
+        print(json.dumps(output_json, indent=2))
         return output_json
